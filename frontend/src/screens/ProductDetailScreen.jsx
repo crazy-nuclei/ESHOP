e7d2a2756d7d775943a486f59519e96f8cc38166
Aayush <aayush.551a@gmail.com>
2021-06-29 14:42:50 +0530

    Added basic backend

--- frontend/src/screens/ProductDetailScreen.jsx
+++ frontend/src/screens/ProductDetailScreen.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductDetailScreen = ({ match }) => {
 
-    const product = products.find(p => match.params.id === p._id);
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [match.params.id]);
     return <>
         <Link className='btn btn-light my-3' to='/'> Go Back
         </Link>
